defe8b68b2da43e3d848d046eaa9f4e20057e09b
xarvh <francesco.orsenigo@adslot.com>
2013-07-13 22:09:57 +1000

    rearrange qtile config to use main(qtile)

--- qtile/config.py
+++ qtile/config.py
@@ -16,148 +16,15 @@ from libqtile.command import lazy
 from libqtile import layout, bar, widget, hook
 
 
-normal = ['mod4']
-heavy = ['mod4', 'mod1']
-
-
-
 #
-# keys and groups
+# exposed globals
 #
-xterm = 'xterm -fn "*-fixed-*-*-*-20-*" '
-commands = {
-  'b': 'chromium-browser',
-  'v': 'gvim',
-  't': '_tango_newsletter',
-  'c': xterm + '-e coffee',
-  'a': xterm + '-e alsamixer',
-  'F12': 'key_u_all',
-  'Return': xterm,
-
-  'equal': 'amixer -c 0 -q set Master 2dB+',
-  'minus': 'amixer -c 0 -q set Master 2dB-',
-}
-
-heavy_commands = {
-  's': 'setxkbmap -layout se',
-  'g': 'setxkbmap -layout gr',
-  '1': 'setxkbmap -layout it',
-  '0': 'setxkbmap -layout us',
-}
-
-keys = [
-  Key(heavy,  "q",              lazy.shutdown()),
-  Key(normal, "j",              lazy.layout.switchdown(0)),
-  Key(heavy,  "j",              lazy.layout.client_to_stack(0)),
-  Key(normal, "k",              lazy.layout.switchdown(1)),
-  Key(heavy,  "k",              lazy.layout.client_to_stack(1)),
-  Key(normal, "h",              lazy.screen.prevgroup()),
-  Key(normal, "l",              lazy.screen.nextgroup()),
-  Key(normal, "space",          lazy.screen.togglegroup()),
-  Key(normal, "semicolon",      lazy.spawncmd()),
-  Key(normal, "t",              lazy.layout.toggle_split()),
-  Key(normal, "r",              lazy.layout.rotate()),
-  Key(normal, "apostrophe",     lazy.nextlayout()),
-  Key(normal, "x",              lazy.window.kill()),
-]
-
-keys += [Key(normal, k, lazy.spawn(v)) for k, v in commands.items()]
-keys += [Key(heavy, k, lazy.spawn(v)) for k, v in heavy_commands.items()]
-
-
-#
-# Screens and bars
-#
-class CustomWindowName(widget.WindowName):
-  def button_press(self, x, y, button):
-    screen = self.bar.screen
-    {
-      1: lambda: screen.group.layout.cmd_switchdown(0), # left mouse, left pane
-      2: lambda: self.qtile.cmd_nextlayout(), # mid mouse, change layout
-      3: lambda: screen.group.layout.cmd_switchdown(1), # right mouse, right pane
-      4: lambda: screen.cmd_nextgroup(), # wheel up
-      5: lambda: screen.cmd_prevgroup(), # wheel down
-    }.get(button, lambda: '')()
-
-
-main_bar = bar.Bar([
-  widget.GroupBox(
-    urgent_alert_method='text',
-    borderwidth=2,
-    padding=1,
-    margin_x=1,
-    margin_y=1,
-    active='FF0000',
-    this_current_screen_border='CC0000',
-    disable_drag=True,
-    inactive='CCCCCC'),
-  widget.Sep(),
-  CustomWindowName(),
-  widget.Sep(),
-  widget.Notify(),
-  widget.Prompt(),
-  widget.Volume(),
-  widget.Systray(),
-  widget.Clock('%m%d %a %I:%M%P'),
-], 20)
-
-
-lower_bar = bar.Bar([
-  CustomWindowName(),
-], 1)
-
-
-screens = [ Screen(top = main_bar, bottom = lower_bar), Screen() ]
-
-
-#
-# Groups
-#
+screens = []
+layouts = []
 groups = []
 
-def main(qtile):
-  dualscreen = len(qtile.conn.pseudoscreens) > 1
-
-  group_def = 'n m comma:, period:. u i o p'
-  if dualscreen:
-    group_def += ' slash:/'
-
-  for key in group_def.split():
-    if len(key) is 1:
-      name = key.upper()
-    else:
-      key, name = key.split(':')
-
-    groups.append(Group(name))
-    keys.append(Key(normal, key, lazy.screen.togglegroup(name)))
-    keys.append(Key(heavy, key, lazy.window.togroup(name)))
-
-  if dualscreen:
-    lazy.group['/'].toScreen(0)
-
-
-#
-# Layouts
-#
-class CustomStack(layout.Stack):
-  def cmd_switchdown(self, offset):
-    offset %= len(self.stacks)
-    if self.currentStackOffset is offset:
-      self.cmd_down()
-    else:
-      self.group.focus(self.stacks[offset].cw, True)
-
-layouts = [
-  CustomStack(stacks=1, border_width=0),
-  CustomStack(stacks=2, border_width=1),
-]
-
-#
-# Mouse
-#
-mouse = [
-#  Click(normal, "Button2", lazy.window.bring_to_front()),
-]
+keys = []
+mouse = []
 
 
 #
@@ -174,25 +41,21 @@ floating_layout = layout.Floating(auto_float_types=[
 #
 # Hooks
 #
-@hook.subscribe.startup
-def startup():
-  pass
-
-
 @hook.subscribe.screen_change
 def restart_on_screen_change(qtile, ev):
   qtile.cmd_restart()
 
 
 #
-# init commands
+# Initialization commands.
+# (These run before the qtile instance is available.)
 #
 screen_names = {
   'pinky': ['LVDS1', 'VGA1'],
   'salad': ['DVI-0', 'DisplayPort-0'],
-}.get(uname()[1], (0, 1))
+  'trash': ['LVDS', 'DFP1'],
+}.get(uname()[1], [0, 1])
 
-print screen_names
 system(' '.join((
   'xrandr',
   '--output {0} --preferred --primary',
@@ -202,4 +65,152 @@ system(' '.join((
 system('setxkbmap -option ctrl:nocaps')
 system('fbsetbg -a ~/.usr/gui/bg.jpg') 
 system('xsetroot -cursor_name left_ptr')
+
+
+#
+# QTile initialization.
+# (main() is run when the qtile instance becomes available.
+# http://docs.qtile.org/en/latest/configuration.html#main
+#
+def main(qtile):
+
+  # TODO: extend for a generic number of screens?
+  dualscreen = not qtile or len(qtile.conn.pseudoscreens) > 1
+
+  # key modifiers
+  normal = ['mod4']
+  strong = ['mod4', 'mod1']
+
+
+  #
+  # commands
+  #
+  term = 'xterm -fn "*-fixed-*-*-*-20-*" '
+  normal_commands = {
+    'b': 'chromium-browser',
+    'v': 'gvim',
+    't': '_tango_newsletter',
+    'c': term + '-e coffee',
+    'a': term + '-e alsamixer',
+    'F12': 'key_u_all',
+    'Return': term,
+
+    'equal': 'amixer -c 0 -q set Master 2dB+',
+    'minus': 'amixer -c 0 -q set Master 2dB-',
+  }
+
+  strong_commands = {
+    's': 'setxkbmap -layout se',
+    'g': 'setxkbmap -layout gr',
+    '1': 'setxkbmap -layout it',
+    '0': 'setxkbmap -layout us',
+  }
+
+  keys.extend([
+    Key(strong, 'q',              lazy.shutdown()),
+    Key(normal, 'j',              lazy.layout.switchdown(0)),
+    Key(strong, 'j',              lazy.layout.client_to_stack(0)),
+    Key(normal, 'k',              lazy.layout.switchdown(1)),
+    Key(strong, 'k',              lazy.layout.client_to_stack(1)),
+    Key(normal, 'h',              lazy.screen.prevgroup()),
+    Key(normal, 'l',              lazy.screen.nextgroup()),
+    Key(normal, 'space',          lazy.screen.togglegroup()),
+    Key(normal, 'semicolon',      lazy.spawncmd()),
+    Key(normal, 't',              lazy.layout.toggle_split()),
+    Key(normal, 'r',              lazy.layout.rotate()),
+    Key(normal, 'apostrophe',     lazy.nextlayout()),
+    Key(normal, 'x',              lazy.window.kill()),
+  ])
+
+  keys.extend([Key(normal, k, lazy.spawn(v)) for k, v in normal_commands.items()])
+  keys.extend([Key(strong, k, lazy.spawn(v)) for k, v in strong_commands.items()])
+
+
+  #
+  # Screens and bars
+  #
+  class CustomWindowName(widget.WindowName):
+    def button_press(self, x, y, button):
+      screen = self.bar.screen
+      {
+        1: lambda: screen.group.layout.cmd_switchdown(0), # left mouse, left pane
+        2: lambda: self.qtile.cmd_nextlayout(), # mid mouse, change layout
+        3: lambda: screen.group.layout.cmd_switchdown(1), # right mouse, right pane
+        4: lambda: screen.cmd_nextgroup(), # wheel up
+        5: lambda: screen.cmd_prevgroup(), # wheel down
+      }.get(button, lambda: '')()
+
+
+  main_bar = bar.Bar([
+    widget.GroupBox(
+      urgent_alert_method='text',
+      borderwidth=2,
+      padding=1,
+      margin_x=1,
+      margin_y=1,
+      active='FF0000',
+      this_current_screen_border='CC0000',
+      disable_drag=True,
+      inactive='CCCCCC'),
+    widget.Sep(),
+    CustomWindowName(),
+    widget.Sep(),
+    widget.Notify(),
+    widget.Prompt(),
+    widget.Volume(),
+    widget.Systray(),
+    widget.Clock('%m%d %a %I:%M%P'),
+  ], 20)
+
+
+  lower_bar = bar.Bar([
+    CustomWindowName(),
+  ], 1)
+
+  screens.extend([ Screen(top = main_bar, bottom = lower_bar), Screen() ])
+
+
+  #
+  # Groups
+  #
+  group_def = 'n m comma:, period:. u i o p'
+  if dualscreen:
+    group_def += ' slash:/'
+
+  for key in group_def.split():
+    if len(key) is 1:
+      name = key.upper()
+    else:
+      key, name = key.split(':')
+
+    groups.append(Group(name))
+    keys.append(Key(normal, key, lazy.screen.togglegroup(name)))
+    keys.append(Key(strong, key, lazy.window.togroup(name)))
+
+  if dualscreen:
+    lazy.group['/'].toScreen(1)
+
+
+  #
+  # Layouts
+  #
+  class CustomStack(layout.Stack):
+    def cmd_switchdown(self, offset):
+      offset %= len(self.stacks)
+      if self.currentStackOffset is offset:
+        self.cmd_down()
+      else:
+        self.group.focus(self.stacks[offset].cw, True)
+
+  layouts.extend([
+    CustomStack(stacks=1, border_width=0),
+    CustomStack(stacks=2, border_width=1),
+  ])
+
+
+#
+# Execute main(), useful to test the file before restarting.
+#
+if __name__ == '__main__':
+  main(None)
 
