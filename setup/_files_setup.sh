9c7e2b267fba9351e33b8915622441db689a482e
xarvh <francesco.orsenigo@gmail.com>
2016-12-05 16:09:39 +1100

    Setup script links qtile config

--- setup/_files_setup.sh
+++ setup/_files_setup.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
+# TODO find a better way to get the path
 cd $(dirname $0)
 cd ..
 DIR=`pwd`
 
 cd
-
 rm -fr Music Pictures Public Templates Videos Documents
 mkdir -p .usr
+mkdir -p .vim/tmp
 
+cd
+cd .config
+ln -s ../.usr/sys/qtile .
+
+cd
 for i in $DIR/_*; do
   n=$(basename $i)
   t=.${n#_}
   rm -rf $t
   ln -fs $i $t 
 done
-
-
-#chmod -w -R $DIR/_fluxbox
-
